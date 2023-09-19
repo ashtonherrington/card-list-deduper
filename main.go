1562cb08b4d33c5426c8a3cacbe31759729b2588
Ashton Herrington <ashtonherrington.ah@gmail.com>
2023-09-18 18:30:15 -0700

    v1 with up to date card files

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	// open CSV file
-	fd, error := os.Open("helvault.csv")
+	fd, error := os.Open("moxfield_haves_2023-05-11-0533Z.csv")
 	if error != nil {
 		fmt.Println(error)
 	}
@@ -21,8 +25,71 @@ func main() {
 	if error != nil {
 		fmt.Println(error)
 	}
-	//var names []string
+	collection := make(map[string]int, 0)
 	for _, record := range records {
-		fmt.Println(record[2] + " " + record[3])
+		numberOfCopies, err := strconv.Atoi(record[0])
+		if err != nil {
+			fmt.Println(error)
+		}
+
+		if collection[record[2]] == 0 {
+			collection[record[2]] = numberOfCopies
+		} else {
+			collection[record[2]] = collection[record[2]] + numberOfCopies
+		}
+
 	}
+
+	file, err := os.Open("card_wishlist_updated_020123.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	wantList := make(map[string]int, 0)
+	r := regexp.MustCompile(`^(.*)(\s\dx)$`)
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	var lineNumber int
+	for scanner.Scan() {
+		lineNumber++
+		lineContents := scanner.Text()
+		if r.MatchString(lineContents) {
+			matches := r.FindStringSubmatch(scanner.Text())
+			if len(matches) == 3 && matches[2] != "" {
+				numberOfCopies, err := strconv.Atoi(matches[2][len(matches[2])-2 : 2])
+				if err != nil {
+					fmt.Println(err)
+				}
+				trimmedCardName := strings.Trim(matches[1], " ,")
+				if trimmedCardName != "" {
+					if _, ok := wantList[trimmedCardName]; ok {
+						fmt.Printf("Duplicate requested card: %s on line number %d\n", trimmedCardName, lineNumber)
+					} else {
+						wantList[trimmedCardName] = numberOfCopies
+					}
+				}
+			}
+		} else {
+			trimmedFullLine := strings.Trim(lineContents, " ,")
+			if trimmedFullLine != "" {
+				if _, ok := wantList[trimmedFullLine]; ok {
+					fmt.Printf("Duplicate requested card: %s on line number %d\n", trimmedFullLine, lineNumber)
+				} else {
+					wantList[trimmedFullLine] = 1
+				}
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	for k, v := range wantList {
+		if collection[k] > 0 {
+			fmt.Printf("You asked for %d copies of %s, but you already own %d\n", v, k, collection[k])
+		}
+	}
+
 }
